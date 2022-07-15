218e24313d7bceafe63d679db455049626087029
Luiza <luiza-97@hotmail.com>
2022-07-15 18:43:30 -0300

    Colocando construtor acima dos métodos

--- src/Banco/ContaCorrente.java
+++ src/Banco/ContaCorrente.java
@@ -3,6 +3,9 @@ package Banco;
 public class ContaCorrente extends Conta {
 	private final double TAXA_SAQUE = 2.5;
 
+	public ContaCorrente(Long agencia, Long numeroConta) {
+		super(agencia, numeroConta);
+	}
 	@Override
 	boolean Deposita(Double Valor) {
 		if (Valor > 0) {
@@ -15,7 +18,5 @@ public class ContaCorrente extends Conta {
 	public boolean Saca(Double Valor) {
 			return super.Saca(Valor + TAXA_SAQUE);			
 	}
-	public ContaCorrente(Long agencia, Long numeroConta) {
-		super(agencia, numeroConta);
-	}
+	
 }
